e80f71e496c68eb07261057f09f55f4970498ff7
Cédric <cedricly@bluewin.ch>
2025-03-24 20:19:19 +0100

    new line of code

--- 1st_draft.py
+++ 1st_draft.py
@@ -10,3 +10,10 @@ data = response.json()
 temperature = data["main"]["temp"]
 humidity = data["main"]["humidity"]
 print(f"Temperature: {temperature}°C, Humidity: {humidity}%")
+
+if temperature > 30 and humidity < 40:
+    print("Water your plant! It's hot and dry.")
+elif data["weather"][0]["main"] == "Rain":
+    print("No need to water. It rained recently.")
+else:
+    print("Your plant is fine for now.")
