9782248c336eef8bbaf1afc94490bcd7678603e5
Cédric <cedricly@bluewin.ch>
2025-04-02 19:14:31 +0200

    add

--- test.py
+++ test.py
@@ -7,19 +7,3 @@ x = 7
 import streamlit as st
 
 st.write("Hooray, we connected everything")
-<<<<<<< HEAD
-
-print('salut')
-
-print("new code")
-
-print("BonApettitJohann")
-
-y = 5
-
-print("KUSO")
-
-
-
-=======
->>>>>>> c8ff60072980494245211972244a76d21495889c
