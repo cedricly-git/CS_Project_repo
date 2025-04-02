e9fa2d5555c74c3fb7ca9162a0217cb0b7f3b368
grace2411 <graceleixin@gmail.com>
2025-04-02 19:09:29 +0200

    grace

--- test.py
+++ test.py
@@ -7,3 +7,19 @@ x = 7
 import streamlit as st
 
 st.write("Hooray, we connected everything")
+<<<<<<< HEAD
+
+print('salut')
+
+print("new code")
+
+print("BonApettitJohann")
+
+y = 5
+
+print("KUSO")
+
+
+
+=======
+>>>>>>> c8ff60072980494245211972244a76d21495889c
