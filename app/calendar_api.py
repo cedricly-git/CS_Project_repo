cc2727262311966b849f70265a7e2346a4ae4cc2
Johann <sorensenjohann9@gmail.com>
2025-05-02 11:20:26 +0200

    calendar api

---
+++ app/calendar_api.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+import datetime
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+
+flow = InstalledAppFlow.from_credentials.json('credentials.json', SCOPES)
