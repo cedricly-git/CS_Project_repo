5e9670524c25b7136db26f47f5f00d6a3d002cfd
Johann <sorensenjohann9@gmail.com>
2025-05-08 10:50:25 +0200

    1

--- old/hardcoded_plant_api.py
+++ old/hardcoded_plant_api.py
@@ -8,4 +8,4 @@ def classify_plant_image(image_file) -> str:
     TEMPORARY STUB: ignore the image and always return the same category
     so we can test the UI without a model.
     """
-    return "Flowering Plants"
+    return "Flower"
