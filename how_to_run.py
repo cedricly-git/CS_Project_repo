c31b0811c9dfaa88ffdd2e6ab2b293bccb5e481b
Johann <sorensenjohann9@gmail.com>
2025-05-15 11:49:47 +0200

    a

--- how_to_run.py
+++ how_to_run.py
@@ -1,22 +1,46 @@
+# TUTORIAL ON HOW TO RUN THE APP AND TRAIN THE MODEL
+
+# how to run the app:
+
 #step 1
-# open new terminal and copy the whole CS_Project_repo into your finder: 
-# git clone https://github.com/cedricly-git/CS_Project_repo.git
+# open terminal on your computer
 
 #step 2
-# open terminal into this folder:
-# cd CS_Project_repo
+# go in the specific folder in your terminal:
+# (e.g. you have it in your Download file)
+# type in your terminal: cd Download
+# press enter
+# type in your terminal: cd Plantelligence
+# press enter
 
-#step 3 (long and not always necessary so try step 4 before)
-# install streamlit and all the libraries(Ithink) from requirements.txt
-# pip install -r requirements.txt
+#step 3
+# install streamlit and all the libraries from requirements.txt
+# type in your terminal: pip install -r requirements.txt
+# press enter
 
 #step 4
-# launch streamlit:
-# streamlit run app/app.py
+# run the app on streamlit:
+# type in your terminal: streamlit run app/app.py
+# press enter
 
 #step 5
-# download a picture of a plant from the internet and have fun with the app
+# download plant images and have fun with the app!
 
 
-# if want to train model
-# python model/train_model.py
+# how to train the model 
+# We have sent you the project with the model trained, however, if you want to train the model and try the app again with the new model, please follow these steps
+
+#step 1
+# delete plant_classifier.keras file in the subfolder 'model'
+# we encourage you to copy and paste it out of the Plantelligence folder to save it before you delete it
+
+#step 2
+# copy step 1 and 2 from before to get in the right folder of your terminal
+
+#step 3
+# type in your terminal: python model/predict.py
+# press enter
+# wait for the model to be trained and automatically saved in the 'model' subfolder (takes a few minutes depending on the processing power of your computer)
+
+#step 4
+# do steps 1 to 5 again from 'how to run the app' to try the app again with the model you just trained
