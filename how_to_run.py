07a2c8bd6a618c940b4c8094a8d4f05ff78f88c2
Johann <sorensenjohann9@gmail.com>
2025-05-16 10:22:33 +0200

    a

--- how_to_run.py
+++ how_to_run.py
@@ -1,4 +1,8 @@
-# TUTORIAL ON HOW TO RUN THE APP AND TRAIN THE MODEL
+# TUTORIAL ON HOW TO RUN THE APP, DOWNLOAD IMAGES AND TRAIN THE MODEL
+
+# --- WARNING ---
+# Our access to the weather API is based on a free trial account, it expires on May 23rd and requests are limited to 1000, if you run the app too many times or use the "Next Week" or "Previous Week" buttons too many times, you will run out of requests and receive an error message. 
+# If this happens before you were able to test the website enough for the grading, please contact us.
 
 # --- how to run the app: ---
 
@@ -54,3 +58,27 @@
 
 #step 5
 # do steps 1 to 5 again from 'how to run the app' to try the app again with the model you just trained
+
+# --- how to download images ---
+# We have sent you the project with the images already downloaded, however, if you want to download the images and try the app again with the new images, please follow these steps
+# However, we recommend to use the images we provided you with, as the ones downloaded from the API are not always the best quality and would need to be filtered
+# if you want to download the images, please follow these steps:
+
+#step 1
+# create a folder called downloaded_plant_images in your Downloads folder
+
+#step 2
+# open terminal on your computer
+
+#step 2
+# go in the specific folder in your terminal:
+# (e.g. you have it in your Download file)
+# type in your terminal: cd Download
+# press enter
+# type in your terminal: cd Plantelligence
+# press enter
+
+#step 3
+# run the Download_images.py script::
+# type in your terminal: pyhton downloading_plant_images_from_Trefle/Download_images.py
+# press enter
